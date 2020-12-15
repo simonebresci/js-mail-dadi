10f43fa1374f2585f499724311216c7385343e6a
Simone Bresci <simone.bresci.05@gmail.com>
2020-12-15 16:03:13 +0100

    codice javascript gioco dadi (NO HTML-CSS)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 
 
 // Controllo email
-var email = prompt('Inserisci la tua mail:');
-var accessoOk = false;;
-accessoOk = controllaAccesso(email);
+// var email = prompt('Inserisci la tua mail:');
+// var accessoOk = false;;
+// accessoOk = controllaAccesso(email);
+
+// Forza gioco in debug
+email='debug';
+accessoOk= true;
 
 if (accessoOk === true){
   giocoDadi(email);
@@ -41,4 +45,97 @@ function printLista(arrayLista) {
 
 function giocoDadi(mailUtente){
   alert('Ciao ' + mailUtente + ', benvenuto nel gioco dei dadi: ');
+
+  // Chiedi quanti lanci vuoi fare
+  var nMatch = prompt('Quanti lanci vuoi fare?');
+  var okInt = false;
+  okInt = verificaInt(nMatch);
+  if (okInt === true){
+
+    // Inizializza  var dadi
+    var dadoUtente = 0, dadoCpu = 0;
+    var nWinUtente = 0, nWinCpu = 0;
+    for( var i=1; i<=nMatch; i++){
+
+    console.log('====  MATCH N:' + i + '  ====');
+    // Lancia dado utente
+    dadoUtente = lanciaDado();
+
+    // Lancia dado cpu
+    dadoCpu = lanciaDado();
+
+    // Stabilisci vincitore lancio
+    console.log('Utente: ' + dadoUtente);
+    console.log('Cpu   : ' + dadoCpu);
+
+    var winner='empty';
+    console.log('THE WINNER IS ...' );
+
+    // Confronta dadi
+    if (dadoUtente > dadoCpu){
+      winner = 'Utente';
+      nWinUtente++;
+    }else if(dadoUtente < dadoCpu){
+      winner = 'CPU';
+      nWinCpu++;
+    }else{
+      winner = 'Pareggio!';
+    }
+
+    console.log(winner);
+
+    console.log('=====================');
+    console.log('                     ');
+
+    }
+
+    // Stabilisci vincitore partita
+    console.log('Utente ha vinto ' + nWinUtente);
+    appo=''
+    for(i=0;i<nWinUtente;i++){ appo+='X';}
+    console.log(appo);
+
+    console.log('Cpu ha vinto ' + nWinCpu);
+    appo=''
+    for(i=0;i<nWinCpu;i++){ appo+='X';}
+    console.log(appo);
+
+    // Confronta dadi
+    if (nWinUtente > nWinCpu){
+      winner = 'Utente';
+    }else if(nWinUtente < nWinCpu){
+      winner = 'CPU';
+    }else{
+      winner = 'NESSUNO';
+    }
+
+    // VINCITORE FINALE
+    console.log(winner + ' HA VINTO LA PARTITA!');
+  }
+
+
+
+
+}
+
+
+function verificaInt(dato){
+  var esito;
+  if (isNaN === true){
+    // Non è un numerico
+    esito='false';
+  }else{
+
+    // TODO: Controlla ulteriore se intero o float
+
+    esito = true;
+  }
+
+  return esito;
+}
+
+function lanciaDado(){
+  var dado;
+  dado = Math.floor(Math.random() * 6 + 1 );
+  return dado;
 }
